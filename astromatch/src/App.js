4f87ed6bb8b63f6b1f6883fc862e0f143e52909d
carolinemartins <caroline.martins.soares09@gmail.com>
2022-12-03 13:40:01 -0300

    adicionar app.js

--- astromatch/src/App.js
+++ astromatch/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Card from './Pages/Card'
+import Match from './Pages/Matchs'
 
 function App() {
+
+  const [pag, setPag] = useState(false)
+
+  const paginaCard = () => {
+    setPag(true)
+  }
+
+  const paginaMatches = () => {
+    setPag(false)
+  }
+
+  //condicao para trocar a pagina
+  const trocaTela = () => {
+    if (pag) {
+      return (
+        <Match matchCard={paginaMatches} />
+      )
+    } else {
+      return (
+        <Card CardMatch={paginaCard} />
+      )
+    }
+  }
+
+  useEffect(() => { trocaTela() })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div>{trocaTela()}</div>
+
     </div>
   );
+
+
 }
 
 export default App;
